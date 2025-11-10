8ee52589a7d1d4c2dc540cbd3f34de4c6aa456cb
lewis-wow <ludvik.prokopec@gmail.com>
2025-11-10 16:50:50 +0100

    small tweaks

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 export const ID_BITS = 4;
 export const K_BUCKET_SIZE = 2;
+
 export const ALPHA = 1;
+
 export const DATA_EXPIRATION_MS = 10 * 60 * 10 * 1_000; // 10 minutes
 export const REPUBLISH_INTERVAL_MS = 10 * 1000; // 10 seconds
